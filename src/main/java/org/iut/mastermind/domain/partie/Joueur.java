ada0d083b987d272458a2df677c421b6867b1537
Stantrh <stanislas.troha8@etu.univ-lorraine.fr>
2024-10-09 20:38:21 +0200

    refacto de Joueur

--- src/main/java/org/iut/mastermind/domain/partie/Joueur.java
+++ src/main/java/org/iut/mastermind/domain/partie/Joueur.java
@@ -1,31 +1,18 @@
 package org.iut.mastermind.domain.partie;
 
-import java.util.Objects;
-
-public class Joueur {
-
-    private final String nom;
+public record Joueur(String nom) {
 
     // constructeur
-    public Joueur(String nom) {
-        this.nom = nom;
-    }
-
-    // getter nom joueur
-    public String getNom() {
-        return this.nom;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(nom);
-    }
 
     // equals
     @Override
     public boolean equals(Object o) {
+        // on vérifie aussi (warning intelliJ) que les objets soient bien de la même classe lors de l'appel de la méthode pour ne pas faire un cast dangereux
+        if (getClass() != o.getClass())
+            return false;
+
         Joueur j = (Joueur) o;
-        return this.nom.equals(j.getNom());
+        return this.nom.equals(j.nom());
     }
 
 }
