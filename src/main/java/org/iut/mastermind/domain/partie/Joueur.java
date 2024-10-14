e72aede3267963353468e4492da2e8953078fbcb
Stantrh <stanislas.troha8@etu.univ-lorraine.fr>
2024-10-14 11:18:06 +0200

    ajout de la méthode getNom utilisée par l'infra pour faire une requête à la base de données dans Joueur.java

--- src/main/java/org/iut/mastermind/domain/partie/Joueur.java
+++ src/main/java/org/iut/mastermind/domain/partie/Joueur.java
@@ -3,6 +3,10 @@ package org.iut.mastermind.domain.partie;
 // on peut la mettre en record car on ne modifie jamais ses attributs, elle est donc immuable
 public record Joueur(String nom) {
 
+    public String getNom(){
+        return nom;
+    }
+
     // equals
     @Override
     public boolean equals(Object o) {
